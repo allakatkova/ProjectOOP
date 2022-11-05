b8b7749fa280367508809383fe5dc15b31a4d3ae
Alla Katkova <allakatkova@bk.ru>
2022-11-05 20:05:47 +0500

    Task 2 begin

--- main.py
+++ main.py
@@ -12,14 +12,23 @@ class Student:
 
 
 class Mentor:
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.name = name
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if (isinstance(student, Student) and course in self.courses_attached and
+                course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -28,12 +37,11 @@ class Mentor:
             return 'Ошибка!'
 
 
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
+def OutputInfoOfStudent(student):
+    print(f"Студент {student.name} {student.surname}, пол {student.gender}")
+    print(f"Пройденные курсы {student.finished_courses}")
+    print(f"Изучаемые курсы {student.courses_in_progress}")
+    print(f"Оценки {student.grades}")
 
 
 if __name__ == '__main__':
@@ -43,29 +51,25 @@ if __name__ == '__main__':
     some_student.grades['Git'] = [10, 10, 10, 10, 10]
     some_student.grades['Python'] = [10, 10]
 
-    print(f"Фамилия {some_student.surname}, имя {some_student.name}, пол {some_student.gender}")
-    print(f"Пройденные курсы {some_student.finished_courses}")
-    print(f"Изучаемые курсы {some_student.courses_in_progress}")
-    print(f"Оценки {some_student.grades}")
+    OutputInfoOfStudent(some_student)
 
     print()
 
     some_student.add_courses('F#')
+    some_student.grades['F#'] = [10, 10, 10, 10, 10]
     print(f"Изучаемые курсы {some_student.finished_courses}")
 
     print()
 
-    mentor_C = Mentor('Константин', 'Павловский')
+    mentor_C = Reviewer('Константин', 'Павловский')
     mentor_C.courses_attached.append('C#')
-    mentor_P = Mentor('Владимир', 'Аркадьев')
+    mentor_P = Reviewer('Владимир', 'Аркадьев')
     mentor_P.courses_attached.append('Python')
 
-    print(mentor_C.name)
-    print(mentor_C.surname)
-    print(mentor_C.courses_attached)
-    print(mentor_P.name)
-    print(mentor_P.surname)
-    print(mentor_P.courses_attached)
+    print(f"Преподаватель {mentor_C.name} {mentor_C.surname}, "
+          f"дисциплина {mentor_C.courses_attached}")
+    print(f"Преподаватель {mentor_P.name} {mentor_P.surname}, "
+          f"дисциплина {mentor_P.courses_attached}")
 
     print()
 
@@ -73,7 +77,4 @@ if __name__ == '__main__':
     mentor_C.rate_hw(some_student, 'C#', 10)
     mentor_P.rate_hw(some_student, 'Python', 10)
 
-    print(f"Фамилия {some_student.surname}, имя {some_student.name}, пол {some_student.gender}")
-    print(f"Пройденные курсы {some_student.finished_courses}")
-    print(f"Изучаемые курсы {some_student.courses_in_progress}")
-    print(f"Оценки {some_student.grades}")
+    OutputInfoOfStudent(some_student)
